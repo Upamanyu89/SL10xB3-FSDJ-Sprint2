bad312afc5156572a1d51b53e50ec47f3140520b
Upamanyu89 <78253616+Upamanyu89@users.noreply.github.com>
2021-03-07 22:50:53 +0530

    Add files via upload
    crud update

--- server.ts
+++ server.ts
@@ -1,10 +1,29 @@
 import express = require('express');
 import bodyParser = require('body-parser');
+import Employees from './Employees';
 const app: express.Application = express();
 const port=3000;
+app.use(bodyParser.json());
 app.get("/",(req,res)=>{
 	res.send("Hello World");
 })
+app.get('/employees',(req,res)=>{
+	res.json({'employees':Employees.getAllEmployees()});
+})
+app.post('/employees',(req,res)=>{
+	
+	Employees.addEmployee(req.body['email'],req.body['name']);
+	res.json({'msg':'Successfully Added'});
+})
+app.put('/employees',(req,res)=>{
+	
+	Employees.updateEmployee(req.body['email'],req.body['name']);
+	res.json({'msg':'Successfully updated'});
+})
+app.delete('/employees',(req,res)=>{
+    Employees.deleteEmployee(req.body['email'],req.body['name']);
+    res.json({ 'msg': 'Successfully deleted'});
+});
 app.listen(port,function(){
 	console.log(`app is started on ${port}`);
 })
